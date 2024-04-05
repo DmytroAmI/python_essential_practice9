c5914beac1da64d040767f57985a2be01b5e39d4
Dmytro Storchak <dmitriystorchak98@gmail.com>
2024-04-05 10:15:34 +0300

    realize task with import specific funcs

---
+++ list_operate.py
@@ -0,0 +1,14 @@
+def append_to_list(list1, value):
+    list1.append(value)
+
+
+def subtract_from_list(list1, value):
+    pass
+
+
+def get_list_value(list1, value):
+    pass
+
+
+def get_list_length(list1):
+    pass
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from area import circle_area
 from calculator import *
 from celsius_fahrenheit import convert_celsius_to_fahrenheit as convert
+from list_operate import append_to_list
 
 area = circle_area(5)
 print("Circle area(radius = 5): ", area)
@@ -12,3 +13,7 @@ print(f"{x} * {y} = {multiply(x, y)}")
 print(f"{x} / {y} = {divide(x, y)}")
 
 print("20 Celsius =", convert(20), "Fahrenheit")
+
+list1 = []
+append_to_list(list1, "Hello World")
+print(list1)
