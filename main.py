0ae323764b4abc93be36f5dda15cd8b2eee21def
Dmytro Storchak <dmitriystorchak98@gmail.com>
2024-04-05 10:07:35 +0300

    realize task with alias

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from area import circle_area
 from calculator import *
+from celsius_fahrenheit import convert_celsius_to_fahrenheit as convert
 
 area = circle_area(5)
 print("Circle area(radius = 5): ", area)
@@ -9,3 +10,5 @@ print(f"{x} + {y} = {add(x, y)}")
 print(f"{x} - {y} = {subtract(x, y)}")
 print(f"{x} * {y} = {multiply(x, y)}")
 print(f"{x} / {y} = {divide(x, y)}")
+
+print("20 Celsius =", convert(20), "Fahrenheit")
