9e4f8b28c635892a938504dcc93faef8957ed663
Dmytro Storchak <dmitriystorchak98@gmail.com>
2024-04-05 10:16:37 +0300

    update code

--- list_operate.py
+++ list_operate.py
@@ -2,13 +2,13 @@ def append_to_list(list1, value):
     list1.append(value)
 
 
-def subtract_from_list(list1, value):
+def subtract_from_list():
     pass
 
 
-def get_list_value(list1, value):
+def get_list_value():
     pass
 
 
-def get_list_length(list1):
+def get_list_length():
     pass
